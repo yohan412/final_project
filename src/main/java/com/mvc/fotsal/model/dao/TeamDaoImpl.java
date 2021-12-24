3e4913274406a523f16f8cd535a15d3db6cdef20
DW_David <akatsukiblue1@gmail.com>
2021-12-24 09:29:20 +0900

    오타 수정

--- src/main/java/com/mvc/fotsal/model/dao/TeamDaoImpl.java
+++ src/main/java/com/mvc/fotsal/model/dao/TeamDaoImpl.java
@@ -6,7 +6,6 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.mvc.fotsal.model.dto.MercenaryDto;
 import com.mvc.fotsal.model.dto.TeamDto;
 
 @Repository
